b7b691d1de5c6dbb61630a3a1961a0b6e0dd2457
Debbie A. Nichols <dnichols346@gmail.com>
2014-05-22 14:30:01 -0700

    Starting my photo app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyPhotoApp::Application.config.secret_token = '7f1c007d6fdb469566b1ac76766733532d31373c6bace1e6ad7e469fcf455e150343c3e91b69850899b6b29f81e20a212c4ffc89a01598895e626ac286028012'
